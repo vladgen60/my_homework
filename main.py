3f6edf373fba135ed198912a2b4453433ae8054c
vladgen60 <153118575+vladgen60@users.noreply.github.com>
2024-09-28 09:50:29 +0300

    28.09.2024 first homework

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# 1st program.
+    # 1st program.
 print(9**0.5*5)
 
-# 2nd program.
+    # 2nd program.
 print(9.99>9.98 and 1000!=1000.1)
 
-# 3rd program.
+    # 3rd program.
 print(2*2+2 == 2*(2+2))
 
-# 4th program
+    # 4th program
 print(int((float('123.456')*10))%10)
 
-# The end
+    # The end
