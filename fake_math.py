436bd524a43289b5ba0e00b8f1cc2a0b016d6bb5
Vladimir <153118575+vladgen60@users.noreply.github.com>
2024-10-26 13:02:06 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+    # Создание модуля fake_math.
+
+def divide(first, second):  # Создаем функцию.
+    if second == 0: # Условие при котором
+        return 'Ошибка' # возвращается ошибка.
+    else:
+        return first / second   # Возвращаем результат деления.
