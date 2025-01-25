7c73d391e4916a6b800ff76860f6c2544fba6a53
Vladimir <153118575+vladgen60@users.noreply.github.com>
2025-01-25 12:04:46 +0300

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = "7324981471:AAFyRXr6Wr2KCkUF0OPovhaELT0dn2cr-xM"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
